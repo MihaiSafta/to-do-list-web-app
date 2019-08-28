842a35da8c46cf6d3306eeda873eb7d4fe2a1d01
Mihai <mihaisaftas.259@gmail.com>
2019-08-28 20:52:48 +0300

    displaying items from api in web pages

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -15,13 +15,43 @@ window.ToDoList = {
         url: ToDoList.API_BASE_URL,
         method: "POST",
         // MIME type
-        contentType: "aplication/json",
+        contentType: "application/json",
         data: JSON.stringify(item)
     }).done(function (response) {
         console.log("Successfully received response");
         console.log(response);
         })
     },
+
+    getItems: function () {
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "GET",
+        }).done(function (response) {
+            console.log("Successfully received response");
+            console.log(response);
+
+            ToDoList.displayItems(JSON.parse(response));
+        })
+    },
+
+    displayItems: function (items) {
+        var tableBodyHtml = '';
+        items.forEach(item => tableBodyHtml +=ToDoList.getItemRow(item) );
+        $('#to-do-items-table tbody').html(tableBodyHtml);
+
+    },
+    getItemRow: function (item) {
+        var formatedDate = new Date(...item.deadline).toLocaleDateString("en-US");
+        return `<tr>
+            <td>${item.description}</td>
+            <td>${formatedDate}</td>
+            <td><input type="checkbox" class="mark-done-checkbox" title="Completed"/></td>
+            <td><a href="#" class="delete-item fa fa-trash"></a> </td>
+        </tr>`
+
+    },
+
     
     bindEvents: function () {
         $('#new-item-form').submit(function (event) {
@@ -32,4 +62,5 @@ window.ToDoList = {
     }
 
 };
+ToDoList.getItems();
 ToDoList.bindEvents();
